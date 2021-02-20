5a5caaa299bceef5887722278990f8ca50eede48
Aaron Hill <aa1ronham@gmail.com>
2021-02-20 15:52:41 -0500

    Delete dead code

--- my_library.c
+++ my_library.c
@@ -7,21 +7,9 @@ void my_dtor() {
     printf("Called destructor\n");
 }
 
-void *my_thread_fn(void *arg) {
-    printf("Thread starting\n");
-    pthread_setspecific(key, (void *) 1);
-    printf("Thread exitingg\n");
-    return NULL;
-}
-
 void my_fn() {
     printf("Creating pthread key!\n");
     pthread_key_create(&key, my_dtor);
     pthread_setspecific(key, (void *) 1);
     printf("Returning from my_fn\n");
-
-    /*pthread_t tid;
-    pthread_create(&tid, NULL, &my_thread_fn, NULL);
-    pthread_join(tid, NULL);
-    printf("Back in my_fn\n");*/
 }
